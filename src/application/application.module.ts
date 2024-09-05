ca73fe5bcc4925cb594a3d32bf523594e02cb882
Studies Thalya <studies.thalya@gmail.com>
2024-09-05 10:17:12 +0000

    ; .

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -3,16 +3,9 @@ import { CostumerController } from './controllers/customer.controller';
 import { DomainModule } from 'src/domain/domain.module';
 import { ManagerController } from './controllers/manager.controller';
 import { AccountController } from './controllers/account.controller';
-import { TesteController } from './controllers/test.controller';
-
 
 @Module({
   imports: [DomainModule],
-  controllers: [
-    CostumerController,
-    ManagerController,
-    AccountController,
-    TesteController,
-  ],
+  controllers: [CostumerController, ManagerController, AccountController],
 })
 export class ApplicationModule {}
