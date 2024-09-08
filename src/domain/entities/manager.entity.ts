56cfa1f080a3b52ffefb7b406d476fa537eceac3
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-09-08 15:43:40 -0300

    z

--- src/domain/entities/manager.entity.ts
+++ src/domain/entities/manager.entity.ts
@@ -1,10 +1,4 @@
-import {
-  Column,
-  Entity,
-  JoinColumn,
-  OneToMany,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { CostumerEntity } from './costumer.entity';
 
 @Entity()
